b893b710155e79b85cb44da1d8c3910afe7530ba
mdzhura <32909428+mdzhura@users.noreply.github.com>
2019-06-06 23:48:53 +0300

    Added comments tests functions

--- BookLibrary/Tests/Services.Tests/AuthorServiceTests.cs
+++ BookLibrary/Tests/Services.Tests/AuthorServiceTests.cs
@@ -50,6 +50,7 @@ namespace Services.Tests
 
         }
 
+        [Fact]
         public void GetAllTest()
         {
             //Arange
--- BookLibrary/Tests/Services.Tests/CommentServiceTests.cs
+++ BookLibrary/Tests/Services.Tests/CommentServiceTests.cs
@@ -67,5 +67,101 @@ namespace Services.Tests
             Assert.NotEmpty(_commentsDto);
             Assert.Equal(3, _commentsDto.Count());
         }
+
+        [Fact]
+        public void GetByIdTest()
+        {
+            String _text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+            //Arange
+            Mock<IUnitOfWork> unitOfWorkMock = new Mock<IUnitOfWork>();
+            Mock<IRepository<Comment>> repositoryMock = new Mock<IRepository<Comment>>();
+            repositoryMock.Setup(repo => repo.Get(It.IsAny<Expression<Func<Comment, bool>>>()))
+                .Returns<Expression<Func<Comment, bool>>>(predicate =>
+                    _comments.Where(predicate.Compile()).AsQueryable());
+            unitOfWorkMock.Setup(getRepo => getRepo.GetRepository<Comment>()).Returns(repositoryMock.Object);
+            CommentService _commentService = new CommentService(unitOfWorkMock.Object);
+            //Act
+            CommentDTO _commentDto = _commentService.Get("id1");
+            //Assert
+            Assert.NotNull(_commentDto);
+            Assert.Equal(_text, _commentDto.Text);
+        }
+
+        [Fact]
+        public void AddTest()
+        {
+            //Arange
+            CommentDTO projectDto = new CommentDTO()
+            {
+                Id = "b0",
+                Text = "Zero"
+            };
+            bool isAdded = false;
+            Mock<IUnitOfWork> unitOfWorkMock = new Mock<IUnitOfWork>();
+            Mock<IRepository<Comment>> repositoryMock = new Mock<IRepository<Comment>>();
+            repositoryMock.Setup(repo => repo.Get(It.IsAny<Expression<Func<Comment, bool>>>()))
+                .Returns<Expression<Func<Comment, bool>>>(predicate =>
+                    _comments.Where(predicate.Compile()).AsQueryable());
+            repositoryMock.Setup(repo => repo.Add(It.IsAny<Comment>())).Callback(() => isAdded = true);
+            unitOfWorkMock.Setup(getRepo => getRepo.GetRepository<Comment>()).Returns(repositoryMock.Object);
+            CommentService _commentService = new CommentService(unitOfWorkMock.Object);
+
+            //Act
+            _commentService.Add(projectDto);
+
+            //Assert
+            Assert.True(isAdded);
+        }
+
+        [Fact]
+        public void RemoveTest()
+        {
+            //Arange
+            bool isRemoved = false;
+            Mock<IUnitOfWork> unitOfWorkMock = new Mock<IUnitOfWork>();
+            Mock<IRepository<Comment>> repositoryMock = new Mock<IRepository<Comment>>();
+            repositoryMock.Setup(repo => repo.Get(It.IsAny<Expression<Func<Comment, bool>>>()))
+                .Returns<Expression<Func<Comment, bool>>>(predicate =>
+                    _comments.Where(predicate.Compile()).AsQueryable());
+            repositoryMock.Setup(repo => repo.Remove(It.IsAny<Comment>())).Callback(() => isRemoved = true);
+            unitOfWorkMock.Setup(getRepo => getRepo.GetRepository<Comment>()).Returns(repositoryMock.Object);
+            CommentService _commentService = new CommentService(unitOfWorkMock.Object);
+
+            //Act
+            _commentService.Remove(_comments[0].Id);
+
+            //Assert
+            Assert.True(isRemoved);
+        }
+
+        [Fact]
+        public void UpdateTest()
+        {
+            String _text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+            //Arange
+            CommentDTO projectDto = new CommentDTO()
+            {
+                Id = "id1",
+                Text = _text
+            };
+            bool isUpdate = false;
+            Mock<IUnitOfWork> unitOfWorkMock = new Mock<IUnitOfWork>();
+            Mock<IRepository<Comment>> repositoryMock = new Mock<IRepository<Comment>>();
+            repositoryMock.Setup(repo => repo.Get(It.IsAny<Expression<Func<Comment, bool>>>()))
+                .Returns<Expression<Func<Comment, bool>>>(predicate =>
+                    _comments.Where(predicate.Compile()).AsQueryable());
+            repositoryMock.Setup(repo => repo.Update(It.Is<Comment>(entity =>
+                    (entity.Id == projectDto.Id) &&
+                    (entity.Text == projectDto.Text))))
+                .Callback(() => isUpdate = true);
+            unitOfWorkMock.Setup(getRepo => getRepo.GetRepository<Comment>()).Returns(repositoryMock.Object);
+            CommentService _commentService = new CommentService(unitOfWorkMock.Object);
+
+            //Act
+            _commentService.Update(projectDto);
+
+            //Assert
+            Assert.True(isUpdate);
+        }
     }
 }
